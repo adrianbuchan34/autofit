18c91c468abf648edf6be171fdf90dcb4386c9ef
Adrian Buschan <adrianbuchan34@gmail.com>
2025-09-26 18:24:14 +0200

    fix: handle division by zero

--- test_app.py
+++ test_app.py
@@ -3,3 +3,8 @@ def test_mul():
     from app import mul
     assert mul(2, 3) == 6
 # added at 2025-09-26T18:18:04.398723
+
+def test_mul():
+    from app import mul
+    assert mul(2, 3) == 6
+# added at 2025-09-26T18:24:14.497895
